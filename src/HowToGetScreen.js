07e965b1de684c365f2bd999fbc2f20510c3963b
thiagocoral <thi.coral@gmail.com>
2019-09-09 21:15:21 -0300

    botão abrir endereço no Maps

--- src/HowToGetScreen.js
+++ src/HowToGetScreen.js
@@ -1,25 +1,54 @@
 import React from 'react';
-import { View, StyleSheet, Text, Dimensions } from 'react-native';
+import { View, StyleSheet, Text, Dimensions, TouchableOpacity, Linking } from 'react-native';
 import Background from './components/Background';
 import ScaledImg from './components/ScaledImg';
 import { ScrollView } from 'react-native-gesture-handler';
 
 const HowToGetScreen = ({ navigation }) => {
+
+
+  
+
   return (
-    <Background>
+    
+    <Background >
+
+
+
       <ScrollView>
-      <Text>
+       
+      <View style={styles.Odin}>
+      <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              var url = "https://goo.gl/maps/SzpjLQVn5BrkTNKa6";
+              Linking.canOpenURL(url).then(supported => {
+                  if (!supported) {
+                      console.log('Cant handle url: ' + url);
+                  } else {
+                      return Linking.openURL(url);
+                  }
+              }).catch(err => console.error('An error occurred', err)); 
+            }}
+          >
+            <Text style={styles.stext}>
+              Abrir no google maps
+            </Text>
+          </TouchableOpacity>
+      <Text >
+        
       Local:Campus	Santo	André  Avenida	dos	Estados, 5001	-	Bairro	Santa Terezinha	-	Santo	André CEP:	09210-580
-      </Text>
+      </Text></View>
+      
       <Text style={styles.bigText}>
         Transporte público
       </Text>
           <View style={styles.Odin}>
         <ScaledImg
           source={require('../assets/foto-mapa.png')}
-          width={Dimensions.get('window').width * .6}
+          width={Dimensions.get('window').width * .7}
         />
-        <Text>*pegar o trem até a estação Celso Daniel
+        <Text>*Pegar o trem até a estação Celso Daniel
         </Text>
         <Text>
           *Descer a escada dentro do terminal
@@ -29,7 +58,7 @@ const HowToGetScreen = ({ navigation }) => {
         <View style={styles.Odin}>
         <ScaledImg
           source={require('../assets/foto-mapa.png')}
-          width={Dimensions.get('window').width * .6}
+          width={Dimensions.get('window').width * .7}
         />
         <Text>*Virar a direita e suba a rampa</Text>
         </View>
@@ -37,17 +66,17 @@ const HowToGetScreen = ({ navigation }) => {
         <View style={styles.Odin}>
         <ScaledImg
           source={require('../assets/foto-mapa.png')}
-          width={Dimensions.get('window').width * .6}
+          width={Dimensions.get('window').width * .7}
         />
-        <Text>Você pode escolher entre ir de fretado ou andando</Text>
+        <Text>*Você pode escolher entre ir de fretado ou andando</Text>
         </View>
 
         <View style={styles.Odin}>
         <ScaledImg
           source={require('../assets/foto-mapa.png')}
-          width={Dimensions.get('window').width * .6}
+          width={Dimensions.get('window').width * .7}
         />
-        <Text>textinho4</Text>
+        <Text>texto 4</Text>
         </View>
         </ScrollView>
     </Background>
@@ -59,13 +88,15 @@ const styles = StyleSheet.create({
     position: 'relative',
     alignItems: 'center',
     justifyContent: 'space-around',
-    flex: 0
+    flex: 1
   },
   button: {
+    position:'relative',
     backgroundColor: '#806cfa',
     height: 90,
     width: '46%',
-    flex: 0,
+    flex: 1,
+    marginTop:10,
     marginBottom: 10,
     borderRadius: 20,
     alignItems: 'center',

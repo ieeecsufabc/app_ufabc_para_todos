4d2e00659228f490767efeca447ff38197dbd32f
MatheusR42 <matheus.araujo.r42@gmail.com>
2019-09-18 20:43:02 -0300

    MapsScreen with ufabc image

--- src/MapsScreen.js
+++ src/MapsScreen.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import { View, StyleSheet, Text, TouchableOpacity, Dimensions } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, Dimensions, SafeAreaView , Image, View } from 'react-native';
+import ReactNativeZoomableView from '@dudigital/react-native-zoomable-view/src/ReactNativeZoomableView';
 import Background from './components/Background';
-import ScaledImg from './components/ScaledImg';
+const mapImage = require('../assets/ufabc-View2.png');
+
+const calcImageHeight = (windowWidth) => {
+  const { width, height } = Image.resolveAssetSource(mapImage);
+
+  return height * (windowWidth / width)
+}
+
+const windowWidth = Dimensions.get('window').width;
+const imageHeight = calcImageHeight(windowWidth);
 
 const MapsScreen = ({ navigation }) => {
   return (
     <Background>
-      <View style={styles.Odin}>  
-        <ScaledImg
-          source={require('../assets/ufabc-View2.png')}
-          width={Dimensions.get('window').width}
-        />
+      <SafeAreaView style={{flex: 1}}>
+        <ReactNativeZoomableView
+          maxZoom={2}
+          minZoom={.9}
+          zoomStep={.5}
+          initialZoom={.9}
+          bindToBorders={false}
+          doubleTapDelay={0}
+          longPressDuration={1100}
+          style={styles.Odin}
+        > 
+          <Image
+            source={mapImage}
+            style={{
+              flex:0,
+              width: windowWidth,
+              height: imageHeight,
+              resizeMode: 'center'
+            }}
+            resizeMode="contain"
+          />
 
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate('Ginasio')}
-        >
-          <Text style={styles.stext}>
-              Ginásioo
-          </Text>
-        </TouchableOpacity>
-      </View>
+          <TouchableOpacity
+            style={styles.btnBlocoA}
+            onPress={() => navigation.navigate('BlocoA')}
+          >
+            <Text style={styles.stext}>
+                Bloco A
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.btnGinasio}
+            onPress={() => navigation.navigate('Ginasio')}
+          >
+            <Text style={styles.stext}>
+              Ginásio
+            </Text>
+          </TouchableOpacity>
+        </ReactNativeZoomableView>
+      </SafeAreaView>
     </Background>
   );
 };
@@ -28,24 +64,37 @@ const MapsScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   Odin: {
     position: 'relative',
-    alignItems: 'center',
-    marginBottom: 5,
-    justifyContent: 'space-evenly',
-    flex: 1
-  },
-  button: {
-    backgroundColor: 'red',
-    height: '28%',
-    width: '80%',
     flex: 0,
-    marginBottom: 10,
-    alignItems: 'center',
+    width: windowWidth,
+    height: imageHeight,
     justifyContent: 'center'
   },
-  boxbutton: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-around'
+  btnBlocoA: {
+    position: 'absolute',
+    backgroundColor: 'transparent',
+    height: '20%',
+    width: '65%',
+    top: '22%',
+    left: '14%',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    transform: [
+      {rotate: '-33deg'}
+    ]
+  },
+  btnGinasio: {
+    position: 'absolute',
+    backgroundColor: 'transparent',
+    // backgroundColor: 'rgba(255, 0, 0, .7)',
+    height: '23%',
+    width: '35%',
+    top: '42%',
+    left: '45%',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    transform: [
+      {rotate: '-40deg'}
+    ]
   },
   stext: {
     fontFamily: 'IsidoraSansAlt-Bold',
@@ -55,7 +104,6 @@ const styles = StyleSheet.create({
     width: '80%',
     color: 'green',
     backgroundColor: 'white',
-    marginTop: 2,
     borderRadius: 5 
   }
 });

56ad46e3f5645f750ad2c5ed89c41a033306407e
MatheusR42 <matheus.araujo.r42@gmail.com>
2019-09-18 22:54:39 -0300

    feat: sortStands

--- src/ListScreen.js
+++ src/ListScreen.js
@@ -1,27 +1,44 @@
 import React from 'react';
-import { StyleSheet, FlatList, View } from 'react-native';
-import StandItem from './components/ListComponents/StandItem'
-import Background from './components/Background'
-import { allMapItems } from './data/markers';
+import {StyleSheet, FlatList, View} from 'react-native';
+import StandItem from './components/ListComponents/StandItem';
+import Background from './components/Background';
+import {allMapItems} from './data/markers';
 
-const ListScreen = ({ navigation }) => {
+const itemsSorted = [...allMapItems].sort((a, b) => {
+  const x = a.description.toLowerCase();
+  const y = b.description.toLowerCase();
+  
+  if (x < y) {
+    return -1;
+  }
+  if (x > y) {
+    return 1;
+  }
+  return 0;
+});
+
+const ListScreen = ({navigation}) => {
   return (
     <Background>
       <View style={styles.container}>
         <FlatList
           keyExtractor={(_item, index) => index.toString()}
-          data={allMapItems}
-          renderItem={({ item: { name, description, builder, top, left, width, height } }) => {
-            return <StandItem
-              title={description}
-              builder={builder}
-              standNumber={name}
-              navigation={navigation}
-              top={top}
-              left={left}
-              width={width}
-              height={height}
-            />;
+          data={itemsSorted}
+          renderItem={({
+            item: {name, description, builder, top, left, width, height},
+          }) => {
+            return (
+              <StandItem
+                title={description}
+                builder={builder}
+                standNumber={name}
+                navigation={navigation}
+                top={top}
+                left={left}
+                width={width}
+                height={height}
+              />
+            );
           }}
         />
       </View>
@@ -31,8 +48,8 @@ const ListScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   container: {
-    paddingHorizontal: 10
-  }
+    paddingHorizontal: 10,
+  },
 });
 
 export default ListScreen;

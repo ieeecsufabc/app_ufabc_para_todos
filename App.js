28190ab38b7bb8f3b4292abf1ea90a8f010b5603
MatheusR42 <matheus.araujo.r42@gmail.com>
2019-09-18 23:24:50 -0300

    delete unused fonts

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import {createStackNavigator, createAppContainer} from 'react-navigation';
 import HomeScreen from './src/HomeScreen';
 import ListScreen from './src/ListScreen';
 import EventsScreen from './src/EventsScreen';
 import Map from './src/components/Map';
 import Map2 from './src/components/Map2';
 import HowToGetScreen from './src/HowToGetScreen';
-import MapsScreen from './src/MapsScreen'; 
+import MapsScreen from './src/MapsScreen';
 import AboutScreen from './src/AboutScreen';
 import IEEEScreen from './src/IEEEScreen';
 import ItinerarioScreen from './src/ItinerarioScreen';
@@ -17,20 +17,20 @@ const navigator = createStackNavigator(
     Home: HomeScreen,
     List: ListScreen,
     Eventos: EventsScreen,
-    Mapas: MapsScreen,     //precisamos colocar um item na lista para uma lista de stands e tals, para que dessa lista a pessoa consiga acessar o mapa
+    Mapas: MapsScreen, //precisamos colocar um item na lista para uma lista de stands e tals, para que dessa lista a pessoa consiga acessar o mapa
     BlocoA: Map,
-    Ginasio: Map2,     //precisamos colocar um item na lista para uma lista de stands e tals, para que dessa lista a pessoa consiga acessar o mapa
+    Ginasio: Map2, //precisamos colocar um item na lista para uma lista de stands e tals, para que dessa lista a pessoa consiga acessar o mapa
     ComoChegar: HowToGetScreen,
     About: AboutScreen,
     IEEE: IEEEScreen,
-    Itinerario: ItinerarioScreen
+    Itinerario: ItinerarioScreen,
   },
   {
     initialRouteName: 'Home',
     defaultNavigationOptions: {
-      title: 'UFABC PARA TODOS - 2019'
-    }
-  }
+      title: 'UFABC PARA TODOS - 2019',
+    },
+  },
 );
 
 const AppContainer = createAppContainer(navigator);
@@ -41,17 +41,16 @@ export default class App extends React.Component {
   };
   async componentDidMount() {
     await Font.loadAsync({
-      'ScriptoramaMarkdownJF': require('./assets/fonts/ScriptoramaMarkdownJFRegular.ttf'),
+      ScriptoramaMarkdownJF: require('./assets/fonts/ScriptoramaMarkdownJFRegular.ttf'),
       'IsidoraSansAlt-Bold': require('./assets/fonts/IsidoraSansAlt-Bold.ttf'),
-
     });
-    this.setState({ fontLoaded: true });
+    this.setState({fontLoaded: true});
   }
-  
+
   render() {
     if (!this.state.fontLoaded) {
-    return null; //aqui poderia adicionar algum tipo de loading
+      return null; //aqui poderia adicionar algum tipo de loading
     }
-      return <AppContainer />;
-    }
+    return <AppContainer />;
+  }
 }
